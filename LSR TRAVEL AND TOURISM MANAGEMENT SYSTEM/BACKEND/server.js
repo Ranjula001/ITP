0b5c2937c82adb4deec41cc680b9dd98ae66aa5e
Ranjula001 <it21011016@my.sliit.lk>
2023-07-08 11:49:40 +0530

    COMPLETE PROJECT

---
+++ LSR TRAVEL AND TOURISM MANAGEMENT SYSTEM/BACKEND/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+});
+
+const connection = mongoose.connection
+connection.once("open", () => {
+    console.log("MongoDB connection established !")
+})
+
+const HotelRouter = require("./routes/Hotels.js");
+const userRouter = require("./routes/auth.js");
+
+app.use("/hotel", HotelRouter); //http://localhost:8070/hotel
+app.use("/users",userRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port : ${PORT}`)
+})
